82d3278397a20b8f97a92da4b7ea93d2d7824266
Jonas Keller <jonaskeller2013@t-online.de>
2026-06-02 17:37:46 +0200

    fix: resolve dev container problems

--- download_datasets.sh
+++ download_datasets.sh
@@ -4,5 +4,6 @@
 wget https://archive.ics.uci.edu/static/public/217/reuter+50+50.zip
 unzip reuter+50+50.zip -d reuters-50-50
 rm reuter+50+50.zip
+mkdir src/datasets/reuters_50_50/data
 mv reuters-50-50/* src/datasets/reuters_50_50/data
 rm -rf reuters-50-50
