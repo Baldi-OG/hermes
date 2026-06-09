23250310283f125cc2a6972be55af7ba76fd4702
Baldi-Desktop <d.baldischwieler@t-online.de>
2026-06-09 18:33:06 +0000

    reformated: Autoformatting

--- src/config.py
+++ src/config.py
@@ -12,7 +12,7 @@ class LLMBackend:
     WEBIS_URL = "https://llm.srv.webis.de/openai/v1/"
     WEBIS_KEY = os.getenv("OLLAMA_KEY")
     # https://llm.srv.webis.de/openai/v1/models
-    WEBIS_MODEL = "qwen3-30b-a3b"   # Number of Parameters: 30.5B in total and 3.3B activated (https://huggingface.co/Qwen/Qwen3-30B-A3B)
+    WEBIS_MODEL = "qwen3-30b-a3b"  # Number of Parameters: 30.5B in total and 3.3B activated (https://huggingface.co/Qwen/Qwen3-30B-A3B)
     WEBIS = "webis"
 
     # TODO: @students: use the one below
@@ -21,13 +21,13 @@ class LLMBackend:
     WEBIS_URL_WEBUI = "https://chat.web.webis.de/openai/"
     WEBIS_KEY_WEBUI = os.getenv("OPENWEBUI_WEBIS_KEY")
     # https://llm.srv.webis.de/openai/v1/models
-    WEBIS_MODEL_WEBUI = "qwen3-30b-a3b"   # Number of Parameters: 30.5B in total and 3.3B activated (https://huggingface.co/Qwen/Qwen3-30B-A3B)
+    WEBIS_MODEL_WEBUI = "qwen3-30b-a3b"  # Number of Parameters: 30.5B in total and 3.3B activated (https://huggingface.co/Qwen/Qwen3-30B-A3B)
     WEBIS_WEBUI = "webis"
 
     # Blablador 
     BLABLADOR_URL = "https://api.helmholtz-blablador.fz-juelich.de/v1/ "
     BLABLADOR_KEY = os.getenv("BLABLADOR_KEY")
-    BLABLADOR_MODEL = "alias-fast" # "02 - Qwen3.5-122B-A10B-FP8"
+    BLABLADOR_MODEL = "alias-fast"  # "02 - Qwen3.5-122B-A10B-FP8"
     BLABLADOR = "blablador"
 
     # OpenAI
@@ -36,5 +36,6 @@ class LLMBackend:
     OPENAI_MODEL = "openai/gpt-5-nano-2025-08-07"  # specify snapshot for consistency: https://platform.openai.com/docs/models/gpt-5-nano (30.10.2025)
     OPENAI_KEY = os.getenv("OPENAI_KEY")
     OPENAI = "openai"
+
 
 CONFIG = LLMBackend()
