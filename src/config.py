bd4c9de6015cf529db5c8dbbd955fac34cf98a3c
Baldi-Desktop <d.baldischwieler@t-online.de>
2026-06-09 18:32:49 +0000

    removed: load_dotenv stub, since it would lead to errors later on

--- src/config.py
+++ src/config.py
@@ -1,12 +1,7 @@
 # added to git since no secrets
 import os
-from pathlib import Path
 
-try:
-    from dotenv import load_dotenv
-except ModuleNotFoundError:
-    def load_dotenv(*args, **kwargs):
-        return False
+from dotenv import load_dotenv
 
 load_dotenv(override=True)
 
